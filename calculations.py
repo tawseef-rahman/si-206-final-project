5443bd6d87d8be05bbe687228d0fb2d542fc4009
tawseef-rahman <tawseefrahman1@gmail.com>
2024-12-16 18:21:51 -0500

    Worked on the calculations for the averages

--- calculations.py
+++ calculations.py
@@ -1,26 +1,33 @@
 import sqlite3
 import csv
 
-def calculate_averages(database):
+def calculate_averages(database, output_file):
     conn = sqlite3.connect(database)
     cursor = conn.cursor()
     
     cursor.execute("""
-                   SELECT c.city, c.latitude, AVG(w.temperature), AVG(w.humidity), AVG(w.air_pressure), AVG(w.uvi) 
+                   SELECT c.city, c.latitude, c.longitude, AVG(w.high_temperature), AVG(w.humidity), AVG(w.air_pressure), AVG(w.wind_speed) 
                    FROM Cities c 
-                   JOIN Weather w on c.id = w.city_id 
+                   JOIN Weather w ON c.id = w.city_id 
                    GROUP BY c.city
                    """)
     rows = cursor.fetchall()
     conn.close()
     
-    with open("calculations.csv", 'w', newline = '') as file:
+    header_row = ["City", "Latitude", "Longitude", "Average High Temperature (degrees Fahrenheit)", "Average Humidity (%)", "Average Air Pressure (hectopascals)", "Average Wind Speed (miles/hour)"]
+
+    with open(output_file, 'w', newline = '') as file:
         writer = csv.writer(file)
-        writer.writerow(["City", "Latitude", "Longitude", "Average Temperature (degrees Fahrenheit), Average Humidity (%), Average Air Pressure (hPa), Average UVI"])
-        writer.writerow(rows)
+        writer.writerow(header_row)
+        writer.writerows(rows)
+    
+    print(f"Averages have been calculated and saved to {output_file}")
 
 def main():
     database = "cities_weather_dates.db"
-    calculate_averages(database)
+
+    output_file = "average_calculations.csv"
+    
+    calculate_averages(database, output_file)
 
 main()
