195d865a89404f04d52816677c3a454a6879fd22
tawseef-rahman <tawseefrahman1@gmail.com>
2024-12-16 17:27:54 -0500

    Worked on the average_air_pressure_average_wind_speed_operation function

--- visualizations.py
+++ visualizations.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def average_humidity_average_high_temperature_operation(database, output_file):
+    plt.figure()
+
     conn = sqlite3.connect(database)
     cursor = conn.cursor()
     
@@ -42,11 +44,55 @@ def average_humidity_average_high_temperature_operation(database, output_file):
     plt.savefig(output_file, format='png', dpi=300)
     print(f"Scatter Plot: {title} saved to {output_file}")
 
+def average_air_pressure_average_wind_speed_operation(database, output_file):
+    plt.figure()
+
+    conn = sqlite3.connect(database)
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+                   SELECT c.city, AVG(w.wind_speed), AVG(w.air_pressure) 
+                   FROM Cities c 
+                   JOIN Weather w ON c.id = w.city_id 
+                   GROUP BY c.city
+                   """)
+    rows = cursor.fetchall()
+    conn.close()
+    
+    average_wind_speeds = []
+    average_air_pressures = []
+    
+    for row in rows:
+        average_wind_speeds.append(row[1])
+        average_air_pressures.append(row[2])
+    
+    average_wind_speeds = np.array(average_wind_speeds, dtype=np.float64)
+    average_air_pressures = np.array(average_air_pressures, dtype=np.float64)
+    
+    plt.scatter(average_wind_speeds, average_air_pressures, color="red", label="(Average Wind Speed, Average Air Pressure)")
+    
+    slope, intercept = np.polyfit(average_wind_speeds, average_air_pressures, 1)
+    equation = f"y = {slope:.2f}x + {intercept:.2f}"
+    plt.plot(average_wind_speeds, slope * average_wind_speeds + intercept, color="green", label=f"Trendline: {equation}")
+    
+    x_label = "Average Wind Speed (miles/hour)"
+    y_label = "Average Air Pressure (hectopascal)"
+    title = "Average Air Pressure vs. Average Wind Speed (12/16/24-12/20/24)"
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.legend(loc='lower left', prop={'size': 7})
+    
+    plt.savefig(output_file, format='png', dpi=300)
+    print(f"Scatter Plot: {title} saved to {output_file}")
+
 def main():
     database = "cities_weather_dates.db"
     
     average_humidity_average_high_temperature_file = "average_humidity_vs_average_high_temperature.png"
+    average_air_pressure_average_wind_speed_file = "average_air_pressure_vs_average_wind_speed.png"
 
     average_humidity_average_high_temperature_operation(database, average_humidity_average_high_temperature_file)
+    average_air_pressure_average_wind_speed_operation(database, average_air_pressure_average_wind_speed_file)
 
 main()
